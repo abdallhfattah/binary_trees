467b2a86ddd0ae6f950448e0f78b32ce9e03c1af
abdallhfattah <abdallh.fattah33@gmail.com>
2024-04-09 15:38:37 +0200

    betty checks

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,21 +1,28 @@
 #include "binary_trees.h"
 /**
- * binary_tree_nodes - function that returns number of nodes in a tree with children
+ * binary_tree_nodes - function that returns number of nodes in a tree
  * @tree: tree to check
  * Return: number of nodes with children
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
-    size_t l = 0;
-    size_t r = 0;
+	size_t l = 0;
+	size_t r = 0;
 
-    l = (tree->left != NULL || tree->right != NULL ? binary_tree_nodes(tree->left) + 1 : 0);
-    r = (tree->left != NULL || tree->right != NULL ? binary_tree_nodes(tree->right)+ 1: 0);
-    return (l + r) / 2;
+	l = (tree->left != NULL ||
+				 tree->right != NULL
+			 ? binary_tree_nodes(tree->left) + 1
+			 : 0);
+	r = (tree->left != NULL ||
+				 tree->right != NULL
+			 ? binary_tree_nodes(tree->right) + 1
+			 : 0);
+
+	return ((l + r) / 2);
 }
