7937bc62d751264c4fb472bbfb896f118d22d992
abdallhfattah <abdallh.fattah33@gmail.com>
2024-04-09 16:18:39 +0200

    moved declaration of vars

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,6 +8,7 @@
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 
+	size_t l = 0, r = 0;
 	if (tree == NULL)
 	{
 		return (0);
@@ -17,9 +18,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	{
 		return (1);
 	}
-
-	size_t l = 0;
-	size_t r = 0;
 
 	l = binary_tree_leaves(tree->left);
 	r = binary_tree_leaves(tree->right);
