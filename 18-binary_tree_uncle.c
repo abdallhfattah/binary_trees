ab163a4aaf4b98d751d269b931bddf1896dc85d2
abdallhfattah <abdallh.fattah33@gmail.com>
2024-04-09 16:22:25 +0200

    betty fixs

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -8,11 +8,11 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-    if (!node || !node->parent || !node->parent->parent || !node->parent->parent->left)
-    {
-        return (NULL);
-    }
-    if (node->parent->parent->left == node->parent)
-        return (node->parent->parent->right);
-    return (node->parent->parent->left);
+	if (!node || !node->parent || !node->parent->parent)
+	{
+		return (NULL);
+	}
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 }
