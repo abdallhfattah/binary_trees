da6b4b1b26cff04b4017f6830ea28ba73e173c0a
abdallhfattah <abdallh.fattah33@gmail.com>
2024-04-09 14:26:44 +0200

    binary tree size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - gets the depth of the node
+ * @tree: the node to get its depth
+ * Return: depth
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+	{
+		return (0);
+	}
+    size_t l = 0;
+    size_t r = 0;
+    l = (tree ? 1 + binary_tree_size(tree->left) : 0);
+    r = (tree ? 1 + binary_tree_size(tree->right) : 0);
+    return (r+l+1);
+}
