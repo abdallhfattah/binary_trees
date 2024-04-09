a455e9b416b17e021903c429cc3099c3d488fa6b
abdallhfattah <abdallh.fattah33@gmail.com>
2024-04-09 16:26:36 +0200

    counting using simple counter

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -6,23 +6,16 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t l = 0;
-	size_t r = 0;
+	size_t count = 0;
 
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
+	count += (tree->left || tree->right);
+	count += binary_tree_nodes(tree->left);
+	count += binary_tree_nodes(tree->right);
 
-	l = (tree->left != NULL ||
-				 tree->right != NULL
-			 ? binary_tree_nodes(tree->left) + 1
-			 : 0);
-	r = (tree->left != NULL ||
-				 tree->right != NULL
-			 ? binary_tree_nodes(tree->right) + 1
-			 : 0);
-
-	return ((l + r) / 2);
+	return (count);
 }
