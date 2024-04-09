922fde7687fb524683e0d8a153ecde40e683cd94
abdallhfattah <abdallh.fattah33@gmail.com>
2024-04-09 16:14:06 +0200

    tryging sto* code

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,23 +1,22 @@
 #include "binary_trees.h"
-
 /**
- * binary_tree_size - gets the size of the tree
- * @tree: the node to get its depth
- * Return: size
+ * binary_tree_size - function that return the size of a tree
+ * @tree: tree to check
+ * Return: size of tree
  */
-
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	size_t size = 0, r = 0, l = 0;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-
-	size_t l = 0;
-	size_t r = 0;
-
-	l = binary_tree_size(tree->left);
-	r = binary_tree_size(tree->right);
-
-	return (r + l + 1);
+	else
+	{
+		l = binary_tree_size(tree->left);
+		r = binary_tree_size(tree->right);
+		size = r + l + 1;
+	}
+	return (size);
 }
