bc4cc6a7e04a575639d548da9ca2900265ff9c14
abdallhfattah <abdallh.fattah33@gmail.com>
2024-04-09 13:43:53 +0200

    postorder traversing throw tree with function parameter

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - post-order traversal "dfs"
+ * @tree: tree to go through
+ * @func: function to use
+ * Return: void
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (tree == NULL || func == NULL) return;
+    else
+    {
+        binary_tree_postorder(tree->left, func);
+        binary_tree_postorder(tree->right, func);
+    }
+    func(tree->n);
+}
