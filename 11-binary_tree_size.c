4dc01cbbe5ea9cef2acd22440c0f03f4f08e32cf
abdallhfattah <abdallh.fattah33@gmail.com>
2024-04-09 14:52:56 +0200

    modified multiple adding of same node

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -14,7 +14,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	}
     size_t l = 0;
     size_t r = 0;
-    l = (tree ? 1 + binary_tree_size(tree->left) : 0);
-    r = (tree ? 1 + binary_tree_size(tree->right) : 0);
+    l = (tree ? binary_tree_size(tree->left) : 0);
+    r = (tree ? binary_tree_size(tree->right) : 0);
     return (r+l+1);
 }
